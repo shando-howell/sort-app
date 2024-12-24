f4dabb405de787253d454e51d5edf8c286d47190
shandohowell <shandohowell94@gmail.com>
2024-12-24 09:29:53 -0500

    Code refactored to sort an array of numbers.

--- build/index.js
+++ build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('Sorting App V3');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 50, -5, 0]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Sorting App V3');
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([10, 50, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
